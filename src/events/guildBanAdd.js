a05103a38f8c473f3d0b7756a8336858ff8abfb7
void* <voidp@protonmail.com>
2018-11-15 20:17:03 +0100

    Randall can now retrieve ban reasons and outputs them to the log channel

--- src/events/guildBanAdd.js
+++ src/events/guildBanAdd.js
@@ -1,18 +1,23 @@
 const config = require("./../config.json");
 
 exports.run = (client, guild, user) => {
+  guild.fetchAuditLogs({type: 'MEMBER_BAN_ADD'}).then((audit) => {
     client.guilds.get(config.settings.log_guild).channels.get(config.settings.log_channel).send({
-        embed:{
-          color: 16711680,
-          author: {
-            name: guild.name,
-            icon_url: guild.iconURL()
-          },
-          description: `**${user.username} [${user.id}]** was banned from **${guild.name}**`,
-          thumbnail: {
-            url: user.avatarURL()
-          },
-          timestamp: new Date()
-        }
-      });
+      embed:{
+        color: 16711680,
+        author: {
+          name: guild.name,
+          icon_url: guild.iconURL()
+        },
+        description: `**${user.username} [${user.id}]** was banned from **${guild.name}**` +
+          `\n\n__**Reason**__\n${(audit.entries.first().reason) ?
+            audit.entries.first().reason :
+            'no reason provided'}`,
+        thumbnail: {
+          url: user.avatarURL()
+        },
+        timestamp: new Date()
+      }
+    });
+  }).catch(console.error);
 }
