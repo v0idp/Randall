ae2c6c149c9f01299d3de666006f1e99d22e011b
void* <voidp@protonmail.com>
2019-03-01 17:15:11 +0100

    fixed ready console log

--- src/events/guildBanRemove.js
+++ src/events/guildBanRemove.js
@@ -1,3 +1,4 @@
 exports.run = (client, guild, user) => {
     client.db.removeBan(guild, user);
+    // make notification to mod server/channel that user got unbanned
 }
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 exports.run = (client) => {
-    console.log(`BotClient ready and logged in as ${client.user.tag} (${client.user.id}). Prefix set to ${client.commandPrefix}. Use ${this.commandPrefix}help to view the commands list!`);
+    console.log(`BotClient ready and logged in as ${client.user.tag} (${client.user.id}). Prefix set to ${client.commandPrefix}. Use ${client.commandPrefix}help to view the commands list!`);
     client.user.setAFK(true);
     client.user.setActivity(`Snitching on ${client.guilds.size} guild(s)`, { type: 'PLAYING' });
     client.isReady = true;
