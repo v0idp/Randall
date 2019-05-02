0b072ebac791bcb8a4f7d5041338ccb66a0efc56
void* <voidp@protonmail.com>
2019-05-03 01:17:13 +0200

    minor change to status message of randall (one guild is the log guild so we subtract from the size of guilds by one)

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 exports.run = (client) => {
     console.log(`BotClient ready and logged in as ${client.user.tag} (${client.user.id}). Prefix set to ${client.commandPrefix}. Use ${client.commandPrefix}help to view the commands list!`);
     client.user.setAFK(true);
-    client.user.setActivity(`Snitching on ${client.guilds.size} guild(s)`, { type: 'PLAYING' });
+    client.user.setActivity(`Snitching on ${client.guilds.size-1} guild(s)`, { type: 'PLAYING' });
     client.isReady = true;
 }
